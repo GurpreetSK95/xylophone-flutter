4e0155d4115e718b43cb418e7f0a392a1f2d3c0b
Gurpreet Singh <gurpreetsk@hotmail.com>
2020-04-13 22:17:51 +0530

    Complete xylophone challenge.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:audioplayers/audio_cache.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(XylophoneApp());
@@ -7,9 +8,42 @@ class XylophoneApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              XylophoneBar(Colors.indigo[900], 1),
+              XylophoneBar(Colors.indigo, 2),
+              XylophoneBar(Colors.blue, 3),
+              XylophoneBar(Colors.green, 4),
+              XylophoneBar(Colors.yellow, 5),
+              XylophoneBar(Colors.orange, 6),
+              XylophoneBar(Colors.red, 7)
+            ],
+          ),
         ),
+      ),
+    );
+  }
+}
+
+class XylophoneBar extends StatelessWidget {
+  final Color color;
+  final int audioFileIndex;
+  final AudioCache audioCache = AudioCache();
+
+  XylophoneBar(this.color, this.audioFileIndex);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: FlatButton(
+
+        color: color,
+        onPressed: () {
+          audioCache.play("note$audioFileIndex.wav");
+        },
       ),
     );
   }
